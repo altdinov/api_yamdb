7b8c8f869c2463315209ab2d34af6c88dc0a59f9
Artur Altdinov <altdinov.artur@yandex.ru>
2023-08-13 22:55:14 +0500

    Добавил импорт данных из csv

--- api_yamdb/reviews/management/commands/import_from_csv.py
+++ api_yamdb/reviews/management/commands/import_from_csv.py
@@ -1,7 +1,9 @@
-from django.core.management.base import BaseCommand
-from reviews.models import Category, Genre, Title, GenreTitle
-from users.models import User
 import csv
+
+from django.core.management.base import BaseCommand
+
+from reviews.models import Category, Genre, GenreTitle, Title
+from users.models import User
 
 
 class Command(BaseCommand):
