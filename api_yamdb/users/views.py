80b8bc7d846e7247bae0a9a77fc221ab9222060f
Илья Журавлев <Nastya14881007@yandex.ru>
2023-08-15 21:23:12 +0300

    Исправления после ревью

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -27,7 +28,7 @@ class UserCreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         ):
             user = User.objects.get(username=request.data.get("username"))
             serializer = UserCreateSerializer(user, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             username = request.data.get("username")
             user = User.objects.get(username=username)
@@ -36,7 +37,6 @@ class UserCreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
                 email=user.email, confirmation_code=confirmation_code
             )
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserReceiveTokenViewSet(mixins.CreateModelMixin,
@@ -59,34 +59,14 @@ class UserReceiveTokenViewSet(mixins.CreateModelMixin,
         return Response(message, status=status.HTTP_200_OK)
 
 
-class UserViewSet(
-    mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet
-):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsSuperUserOrIsAdmin,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("username",)
-
-    @action(
-        detail=False,
-        methods=["get", "patch", "delete"],
-        url_path=r"(?P<username>[\w.@+-]+)",
-        url_name="get_user",
-    )
-    def get_user_by_username(self, request, username):
-        user = get_object_or_404(User, username=username)
-        if request.method == "PATCH":
-            serializer = self.serializer_class(
-                user, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        elif request.method == "DELETE":
-            user.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        serializer = self.serializer_class(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    lookup_field = "username"
+    http_method_names = ["get", "patch", "delete", "post"]
 
     @action(
         detail=False,
